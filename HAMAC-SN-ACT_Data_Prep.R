60d1d9dee667141941d60b8b86d6bfeb8020ee1e
ScriBanana <pwet21@hotmail.fr>
2024-01-11 14:32:37 +0100

    Terminé screening des ACT

--- HAMAC-SN-ACT_Data_Prep.R
+++ HAMAC-SN-ACT_Data_Prep.R
@@ -3,3 +3,85 @@
 #  ACT DATA CLEAN AND MERGE CODE 
 #  A. SCRIBAN - Janvier 2024
 
+setwd("/home/scriban/Dropbox/Thèse/DonneesEtSauvegardes/WorkspaceR/HAMAC")
+setwd("D:/USERS/SergeEtArthur/WorkspaceR/hamac")
+
+
+
+rm(list=ls())
+date()
+
+workd0<-"./0_raw_data/ACT"
+workd1<-"./1_Data_clean_and_merge"
+
+################################################################################
+## A. Concatenation et mise en forme des donnees ACT acquises
+################################################################################
+
+################################################################################
+# A.1. Lecture, concatenation et mise en forme des données ACT
+
+# Concaténation
+filename<- list.files(workd0,full.names = TRUE)
+filename
+ACTACQorig<-read.csv(filename[1],sep=";",header=F,skip=1,na.strings = "NA")
+for(i in 2:length(filename)) {
+  actacq=read.csv(filename[i],sep=";",header=F, skip=1,na.strings = "NA")
+  ACTACQorig=rbind(ACTACQorig, actacq)
+}
+head(ACTACQorig)
+
+# selection des colonnes d'intéret
+ACTACQ <- ACTACQorig[,c(2,3,4,12,13,14,15,7,10,11)]
+names(ACTACQ)=c("IDCOL","DACQ","HACQ","AcX","AcY","AcZ","TMP","ORI","MOD","DT")
+head(ACTACQ)
+
+# Horodatage en une seule colonne
+ACTACQ$DHACQ<-paste(ACTACQ$DACQ,ACTACQ$HACQ)
+ACTACQ$DHACQ <- ifelse (str_length(ACTACQ$DHACQ)==10,paste(ACTACQ$DHACQ," 00:00:00",sep=""),ACTACQ$DHACQ)
+ACTACQ$DHACQ<-as.POSIXct(strptime(ACTACQ$DHACQ,format="%d/%m/%Y %H:%M:%S"),tz="GMT")
+ACTACQ <- ACTACQ[,c(1,11,4,5,6,7,8,9,10)]
+
+# Formatage des données
+ACTACQ$ORI<-as.logical(ACTACQ$ORI == "Collar")
+
+# NA omit et formatage
+dim(ACTACQ)
+ACTACQ<-na.omit(ACTACQ)
+dim(ACTACQ)
+head(ACTACQ)
+table(ACTACQ$IDCOL)
+summary(ACTACQ)
+
+##############################################################################
+# A.2. Check doublons éventuels
+
+dim(ACTACQ)
+dupli<-duplicated(ACTACQ[,c(1:2)])
+summary(dupli)
+ACTACQ<-ACTACQ[dupli==F,]
+table(ACTACQ$IDCOL)
+dim(ACTACQ)
+head(ACTACQ)
+summary(ACTACQ)
+
+
+###############################################################################
+# A.3. Fenêtres temporelle et spatiale
+
+#plot(ACTACQ$LON,ACTACQ$LAT,asp=1)
+dim(ACTACQ)
+ACTACQ<-ACTACQ[ACTACQ$DHACQ>=as.POSIXct("2021-05-09"),]
+
+# boxplot(ACTACQ$TMP)
+# dim(ACTACQ[ACTACQ$TMP<0,])
+# dim(ACTACQ[ACTACQ$TMP>60,])
+# a <- rbind(ACTACQ[ACTACQ$TMP<0,], ACTACQ[ACTACQ$TMP>60,])
+# hist(a$TMP, breaks = 10)
+ACTACQ<-ACTACQ[ACTACQ$TMP>0,]
+ACTACQ<-ACTACQ[ACTACQ$TMP<60,]
+
+
+dim(ACTACQ)
+summary(ACTACQ)
+
