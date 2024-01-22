1e92848ce6b63422950ca3ad94b440f444c51182
ServeurSELMET <calculselmet@gmail.com>
2024-01-22 11:42:10 +0100

    Petits changement à Summary_graph pour le faire fonctionner avec ANX

--- HAMAC-SN-2c-Summary_graph.R
+++ HAMAC-SN-2c-Summary_graph.R
@@ -38,16 +38,16 @@ LOC<-read.table(paste0(workd4,"/HAMAC-SN-HMMDATA.csv"),sep=";",header=T, skip=0,
 #LOC$DHACQ <- as.POSIXct(strptime(LOC$DHACQ, "%Y-%m-%d %H:%M:%S"))
 LOC<-na.omit(LOC) #  PB : je vire les NAs g?n?r?s ? l'importation des dates qui tombent ? 00:00:00
 
-# LOC$DACQ <- substr(LOC$DHACQ,1,10)
-# LOC$DACQ <- as.POSIXct(strptime(LOC$DACQ, "%Y-%m-%d"))
-
-LOC$DACQ <- ymd_hms(LOC$DHACQ)
-LOC<-na.omit(LOC)
-LOC$DACQ <- ymd_hms(LOC$DHACQ)
+LOC$DACQ <- substr(LOC$DHACQ,1,10)
 LOC$DACQ <- as.POSIXct(strptime(LOC$DACQ, "%Y-%m-%d"))
 
-# LIST_NAME<-unique(LOC$IDCOL)
-LIST_NAME<-unique(LOC$ID)
+# LOC$DACQ <- ymd_hms(LOC$DHACQ)
+# LOC<-na.omit(LOC)
+# LOC$DACQ <- ymd_hms(LOC$DHACQ)
+# LOC$DACQ <- as.POSIXct(strptime(LOC$DACQ, "%Y-%m-%d"))
+
+LIST_NAME<-unique(LOC$IDCOL)
+# LIST_NAME<-unique(LOC$ID)
 
 
 ## A.1.2. TRAME preparation
@@ -56,8 +56,8 @@ STUDY_TERM <- min(LOC$DACQ)+ (STUDY_DURATION*86400)
 STUDY_TERM
 PERIOD<-as.data.frame(seq(min(LOC$DACQ),STUDY_TERM, by = 86400))
 colnames(PERIOD)<- "DACQ"
-# PERIOD$DACQ <- as.POSIXct(strptime(PERIOD$DACQ, "%Y-%m-%d"))
-PERIOD$DACQ <- ymd_hms(PERIOD$DACQ)
+PERIOD$DACQ <- as.POSIXct(strptime(PERIOD$DACQ, "%Y-%m-%d"))
+# PERIOD$DACQ <- ymd_hms(PERIOD$DACQ)
 head(PERIOD)
 
 ## Summary graph preparation
@@ -81,8 +81,8 @@ head(GRAPH)
 for(i in 1:length(LIST_NAME)){
 #i=1
   
-  # SUB<- subset(LOC, subset = IDCOL == LIST_NAME[i])
-  SUB<- subset(LOC, subset = ID == LIST_NAME[i])
+  SUB<- subset(LOC, subset = IDCOL == LIST_NAME[i])
+  # SUB<- subset(LOC, subset = ID == LIST_NAME[i])
   str(SUB)
   
   SUB$COUNT<- rep(1,nrow(SUB))
@@ -90,10 +90,10 @@ for(i in 1:length(LIST_NAME)){
   ## A.1.1.  synthetic dataframe  (daily rather than hourly)
   
   DACQ<-na.omit(as.data.frame(unique(SUB$DACQ)))
-  # LON<-as.data.frame(tapply(SUB$LON,SUB$DACQ, mean))
-  # LAT<-as.data.frame(tapply(SUB$LAT,SUB$DACQ, mean))
-  LON<-as.data.frame(tapply(SUB$x,SUB$DACQ, mean))
-  LAT<-as.data.frame(tapply(SUB$y,SUB$DACQ, mean))
+  LON<-as.data.frame(tapply(SUB$LON,SUB$DACQ, mean))
+  LAT<-as.data.frame(tapply(SUB$LAT,SUB$DACQ, mean))
+  # LON<-as.data.frame(tapply(SUB$x,SUB$DACQ, mean))
+  # LAT<-as.data.frame(tapply(SUB$y,SUB$DACQ, mean))
   SUCESS_ABS<-as.data.frame(tapply(SUB$COUNT,SUB$DACQ, sum))
   
   SUB1<-cbind(DACQ,LON,LAT,SUCESS_ABS)
