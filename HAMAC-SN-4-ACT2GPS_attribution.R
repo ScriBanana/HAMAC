31f2d93a7b0ca8e8e457795e8fc435e75fa78f22
ScriBanana <arthur.scriban@cirad.fr>
2024-01-24 16:53:19 +0100

    Parallélisé ACT2GPS_attribution qui prenait des plombes

--- HAMAC-SN-4-ACT2GPS_attribution.R
+++ HAMAC-SN-4-ACT2GPS_attribution.R
@@ -4,12 +4,14 @@
 #  Arthur SCRIBAN & ChatGPT - JANVIER 2024
 
 library(dplyr)
+library(furrr)
 
 setwd("/home/scriban/Dropbox/Thèse/DonneesEtSauvegardes/WorkspaceR/HAMAC")
 setwd("D:/USERS/SergeEtArthur/WorkspaceR/hamac")
 
+# Nettoyer le cache
+rm(list=ls())
 
-# rm(list=ls())
 
 #### Importation données classées par animal
 sourceDir <- "./1_Data_clean_and_merge/"
@@ -22,31 +24,28 @@ GPS_par_anx$DHACQ<-ymd_hms(GPS_par_anx$DHACQ)
 head(GPS_par_anx)
 
 # ACT
-ACT_par_anx <- read.table(
-  paste0(sourceDir, "HAMAC-SN-ACTpANX.csv"),
-  sep=";",header=T, skip=0,na.strings = "N/A")
-ACT_par_anx$DHACQ<-ymd_hms(ACT_par_anx$DHACQ)
-head(ACT_par_anx)
+## Ne marche pas en l'état mais flemme, plutôt runner Animal segmentation
+# ACT_par_anx <- read.table(
+#   paste0(sourceDir, "HAMAC-SN-ACTpANX.csv"),
+#   sep=";",header=T, skip=0,na.strings = "N/A")
+# ACT_par_anx$DHACQ<-ymd_hms(ACT_par_anx$DHACQ)
+# head(ACT_par_anx)
 
 
 # Paramètres 
-delta_t <- 15 # minutes
+nbACTpts <- 5 # Nombre de points ACT autour du point GPS
 noms_col_2_add <- c("AcX", "AcY", "AcZ", "TMP")
 
-# Function to calculate the mean of DATA in a window of delta_t minutes around each timestamp
 calcul_moyenne_accelero <- function(ligne_GPS) {
-  lower_bound <- ligne_GPS$DHACQ - delta_t * 60
-  upper_bound <- ligne_GPS$DHACQ + delta_t * 60
-  
-  pts_dans_fenetre <- subset(ACT_par_anx[[ligne_GPS$ID]],
-                             DHACQ >= lower_bound & DHACQ <= upper_bound)
-  mean_data <- colMeans(pts_dans_fenetre[,noms_col_2_add])
+  closest_rows <- ACT_par_anx[[ligne_GPS$ID]] %>%
+    arrange(abs(DHACQ - ligne_GPS$DHACQ)) %>%
+    slice_head(n = nbACTpts)
+  mean_data <- colMeans(closest_rows[,noms_col_2_add])
   
   return(mean_data)
 }
 
-# Apply the function to calculate mean_data for each row in table1
-# GPS_ACT_par_anx <- GPS_par_anx[1:100,]
+# GPS_ACT_par_anx <- GPS_par_anx[1:1000,]
 GPS_ACT_par_anx <- GPS_par_anx
 colnames(GPS_ACT_par_anx)[colnames(GPS_ACT_par_anx) == 'TMP'] <- "GPS_TMP"
 for (col_name in noms_col_2_add) {
@@ -54,15 +53,25 @@ for (col_name in noms_col_2_add) {
 }
 head(GPS_ACT_par_anx)
 
+plan(multisession, workers = 22) # Début parallelisation sur workers threads
+
 n <- nrow(GPS_ACT_par_anx)
-for (i in 1:n) { # Super long, mais pas r?ussis ? faire un lapply
+result_list <- future_map(1:n, function(i) {
   if (i %% round(n/10) == 0) {
     percentage_completion <- round((i / n) * 100)
     cat(sprintf("Association en cours... %d%%\n", percentage_completion))
   }
-  GPS_ACT_par_anx[i,noms_col_2_add] <- calcul_moyenne_accelero(GPS_ACT_par_anx[i,])
+  calcul_moyenne_accelero(GPS_ACT_par_anx[i, ])
+})
+
+plan(sequential) # Fin parallelisation
+
+for (i in 1:n) {
+  GPS_ACT_par_anx[i, noms_col_2_add] <- result_list[[i]]
 }
+rm(result_list)
 head(GPS_ACT_par_anx)
+
 
 # Enregistrement
 workd1<-"./1_Data_clean_and_merge"
