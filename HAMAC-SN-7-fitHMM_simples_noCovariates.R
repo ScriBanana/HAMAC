af02d63692d64fc2315a3ea1351fb785d418afc5
ScriBanana <arthur.scriban@cirad.fr>
2024-01-25 12:11:32 +0100

    De quoi corriger les données dans le préliminaire sans refaire les bases enregistrées

--- HAMAC-SN-7-fitHMM_simples_noCovariates.R
+++ HAMAC-SN-7-fitHMM_simples_noCovariates.R
@@ -8,6 +8,8 @@ library(moveHMM)
 library(dplyr)
 
 ### ATTENTION : faire tourner le script Préliminaires_Fits au préalable
+# hmmdataFull <- hmmdata
+# hmmdata <- hmmdataFull[hmmdataFull$ID == "VSR21" , ]
 
 head(hmmdata)
 summary(hmmdata)
