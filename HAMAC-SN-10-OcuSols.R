387c683af52021c9cf4b48212a0698cf11b5ad1c
Arthur Scriban <arthur.scriban@cirad.fr>
2024-07-09 17:32:00 +0200

    Changé les couleurs et légendes + ajouté les transferts de fertilité

--- HAMAC-SN-10-OcuSols.R
+++ HAMAC-SN-10-OcuSols.R
@@ -7,13 +7,13 @@
 library(dplyr)
 library(ggplot2)
 
-setwd("/home/scriban/Dropbox/Thèse/DonneesEtSauvegardes/WorkspaceR/HAMAC")
+setwd("~/Dropbox/These/GPS/HAMAC")
 setwd("D:/USERS/SergeEtArthur/WorkspaceR/hamac")
 
 rm(list=ls())
 
 #### Importations données
-cheminDonnees <- "../../../Productions/Articles/Mobi/PartageCGPS/OcuSols/"
+cheminDonnees <- "../PartageCGPS/OcuSols/"
 
 
 ## Importation métadonnées
@@ -75,29 +75,39 @@ write.csv2(OcuSolsLegende, file =  paste0(cheminDonnees, "OcuSolsClasses.csv"),
 #### REPRISE ICI
 
 ## Graphs
-cheminDonnees <- "../../../Productions/Articles/Mobi/PartageCGPS/OcuSols/"
+cheminDonnees <- "../PartageCGPS/OcuSols/"
 OcuSolsLegende <- read.csv2(paste0(cheminDonnees, "OcuSolsClasses.csv"))
 # OcuSolsLegende <- OcuSolsLegende[OcuSolsLegende$ID == "VBT51",]
 
 # Labels pour les différentes variables
-OcuSolsLegende$SES <- factor(
-  OcuSolsLegende$SES,
-  levels = c("SSf", "SSc", "SP"),
-  labels = c("Cold dry season", "Warm dry season", "Rainy season"))
-OcuSolsLegende$DAYTM <- factor(OcuSolsLegende$DAYTM, labels = c("Nighttime", "Daytime"))
-OcuSolsLegende$TRA <- factor(OcuSolsLegende$TRA, labels = c("Resident herds", "Transhuming herds"))
-OcuSolsLegende$VIT <- factor(OcuSolsLegende$VIT, labels = c("Resting", "Foraging", "Travelling"))
-OcuSolsLegende$Legende.finale <- factor(OcuSolsLegende$Legende.finale, labels = c(
-  # "Trees", "Lowlands", "Dwellings", "Bushfields", "Homefields*", "Rivers",
-  # "Fallows*", "Gardens*", "Ponds*", "Rangelands", "Roads*", "Naked ground"))
-  "Other", "Lowlands", "Other", "Bushfields", "Homefields*", "Other",
-  "Fallows*", "Other", "Other", "Rangelands", "Other", "Other"))
+OcuSolsLegende$SES <- factor(OcuSolsLegende$SES,
+                             levels = c("SSf", "SSc", "SP"),
+                             labels = c("Cold dry season", "Warm dry season", "Rainy season"))
+OcuSolsLegende$DAYTM <- factor(OcuSolsLegende$DAYTM,
+                               labels = c("Nighttime", "Daytime"))
+OcuSolsLegende$TRA <- factor(OcuSolsLegende$TRA,
+                             labels = c("Resident herds", "Transhumant herds"))
+OcuSolsLegende$VIT <- factor(OcuSolsLegende$VIT,
+                             labels = c("Resting", "Foraging", "Travelling"))
+OcuSolsLegende$Legende.courte <- factor(OcuSolsLegende$Legende.finale, labels = c(
+  
+  # Arbres           Bas fonds    Bâti     Champs de brousse 
+  # Champs de cases  Cours d'eau  Jachère  Jardins 
+  # Mares            Parcours     Routes   Sol nu 
+  
+  # "Trees", "Lowlands", "Dwellings", "Bushfields",
+  # "Homefields*", "Rivers", "Fallows*", "Gardens*",
+  # "Ponds*", "Rangelands", "Roads*", "Naked ground"))
+  
+  "Cropland trees*", "Lowlands", "Homefields*", "Bushfields",
+  "Homefields*", "Lowlands", "Fallows*", "Other",
+  "Lowlands", "Rangelands", "Other", "Bushfields"))
 
 ## Explo
 # [OcuSolsLegende$TRA == 'Resident herds',]
 ggplot(OcuSolsLegende,
        aes(x = factor(VIT, labels = c("RST", "FRG", "TVL")),
-           fill = factor(Legende.finale, levels = c(
+           fill = factor(Legende.courte, levels = c(
              "Other", "Lowlands", "Rangelands", "Fallows*", "Bushfields", "Homefields*")))) +
   facet_grid(
     TRA ~
@@ -123,21 +133,25 @@ ggplot(OcuSolsLegende,
 
 ### Alternative avec des pourcentages
 OcuSolsLegendePercent <- OcuSolsLegende %>%
-  group_by(TRA, SES, DAYTM, VIT, Legende.finale) %>%
+  group_by(TRA, SES, DAYTM, VIT, Legende.courte) %>%
   summarise(count = n(), .groups = 'drop') %>%
   group_by(TRA, SES, DAYTM) %>%
   mutate(percentage = count / sum(count))
 
 ggplot(OcuSolsLegendePercent,
        aes(x = factor(VIT, labels = c("RST", "FRG", "TVL")), y = percentage,
-           fill = factor(Legende.finale, levels = c(
-         "Other", "Lowlands", "Rangelands", "Fallows*", "Bushfields", "Homefields*")))) +
+           fill = factor(Legende.courte, levels = c(
+             "Other", "Rangelands", "Lowlands", "Cropland trees*",
+             "Fallows*", "Bushfields", "Homefields*")))) +
   facet_grid(
     SES ~ TRA + factor(DAYTM, levels = c("Daytime", "Nighttime")),
     scales = "free_y"
   ) +
   geom_bar(stat = "identity", position = "stack") +
-  scale_fill_brewer(palette = "BrBG") +
+  scale_fill_manual(values = c(
+    "#666666", "#018571", "#80cdc1", "#f6e8c3", "#dfc27d", "#bf812d", "#8c510a"
+    )) +
+  # scale_fill_brewer(palette = "BrBG", direction=-1) +
   theme_minimal() +
   theme(
     panel.grid.major.x = element_blank(),
@@ -145,12 +159,12 @@ ggplot(OcuSolsLegendePercent,
   ) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   labs(x = "Activity state",
-       y = "Percentage of observations in each state",
+       y = "Proportions of observations in each state per case",
        fill = "Land use")
 
 
 ## Figure globale
-ggplot(OcuSolsLegende, aes(x = SES, fill = Legende.finale)) +
+ggplot(OcuSolsLegende, aes(x = SES, fill = Legende.courte)) +
   facet_grid(VIT ~ TRA) + # Ajouter factor et labels
   geom_bar(position = "fill", stat = "count") +
   scale_y_continuous(labels = scales::percent_format()) +
@@ -179,6 +193,39 @@ ggplot(OcuSolsLegende, aes(x =
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
+# Fertility transfers
+PerSESTRA <- OcuSolsLegendePercent %>%
+  group_by(TRA, VIT, SES, Legende.courte, DAYTM) %>%
+  summarise(total_count = sum(count), .groups = 'drop')
+
+CondensPerSESTRA <- PerSESTRA[PerSESTRA$TRA == "Resident herds",]
+CondensPerSESTRA <- CondensPerSESTRA[CondensPerSESTRA$SES != "Rainy season",]
+CondensPerSESTRA <- CondensPerSESTRA %>%
+  group_by(DAYTM, Legende.courte) %>%
+  summarise(total_count = sum(total_count), .groups = 'drop')
 
 
+ggplot(OcuSolsLegendePercent,
+       aes(x = DAYTM, y = percentage,
+           fill = factor(Legende.courte, levels = c(
+             "Other", "Rangelands", "Lowlands", "Cropland trees*",
+             "Fallows*", "Bushfields", "Homefields*")))) +
+  facet_grid(
+    SES ~ TRA,
+    scales = "free_y"
+  ) +
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_manual(values = c(
+    "#666666", "#018571", "#80cdc1", "#f6e8c3", "#dfc27d", "#bf812d", "#8c510a"
+  )) +
+  # scale_fill_brewer(palette = "BrBG", direction=-1) +
+  theme_minimal() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  ) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  labs(x = "Activity state",
+       y = "Proportions of observations in each state per case",
+       fill = "Land use")
 
